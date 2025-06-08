a7087235d6a5565fa596c341fa3f9f9b7ee38ab0
Pratyush <66518297+26pratyush@users.noreply.github.com>
2025-06-08 19:02:17 +0530

    Update 1-a.js

--- 1-a.js
+++ 1-a.js
@@ -1,3 +1,6 @@
+//Write a function translate() that will translate a text i.e, double every consonant and place an occurrence of "o" in between.
+//For example, translate("this is fun") should return the string "tothohisos isos fofunon".
+
 const getMonthNameConverter = () => {
   const monthNames = [
     "January", "February", "March", "April", "May", "June",
