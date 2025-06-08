7359e9680a9359598eb6e02c1f51779aa8b412e6
Pratyush <66518297+26pratyush@users.noreply.github.com>
2025-06-08 21:00:57 +0530

    Update 2-a.js

--- 2-a.js
+++ 2-a.js
@@ -2,22 +2,31 @@
 //If the user enters a number less than 1 or greater than 12 or a non-number, have the function write "Bad Number" in the monthName field.
 //If the user enters a decimal between 1 and 12 (inclusive), strip the decimal portion of the number.
 
-const getMonthNameConverter = () => {
-  const monthNames = [
-    "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-  ];
-  return function(monthInput) {
-    const monthNumber = parseInt(monthInput, 10);
-    return (monthNumber >= 1 && monthNumber <= 12) ? monthNames[monthNumber - 1] : "Bad Number";
-  };
-};
+function monthConverter() 
+{
+  const names = [ "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December" ];
 
-const convertMonth = getMonthNameConverter();
+  return function(month) 
+  {
+    const num = parseInt(month, 10);
+    
+    if (num >= 1 && num <= 12) 
+    {
+      return names[num - 1];
+    } 
+    else {
+      return "Bad Number";
+    }
+  }
+}
 
-console.log(convertMonth("3"));
-console.log(convertMonth("7.9"));
-console.log(convertMonth("0"));
-console.log(convertMonth("13"));
-console.log(convertMonth("abc"));
+const convertMonth = monthConverter();
+
+console.log(convertMonth("3"));     // March
+console.log(convertMonth("7.9"));   // July
+console.log(convertMonth("0"));     // Bad Number
+console.log(convertMonth("13"));    // Bad Number
+console.log(convertMonth("abc"));   // Bad Number
+
     
