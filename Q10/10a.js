c10de083e134212dbd5c61710e6fced771cd649b
Pratyush <66518297+26pratyush@users.noreply.github.com>
2025-06-24 09:53:00 +0530

    Update and rename 10-a.js to Q10/10a.js

---
+++ Q10/10a.js
@@ -0,0 +1,24 @@
+//Write a node.js Express program to create a custom middleware functions for
+//i. Logger
+//ii. No. of time the visitor visited the website (Not Done)
+
+const express = require('express');
+const app = express();
+
+let visitCount = 0;
+
+app.get('/', (req, res) => {
+  visitCount++; // increase count
+  
+  console.log('LOGGED');
+  console.log(`${new Date().toLocaleString()} - ${req.method} ${req.url}`);
+
+  res.send(`
+    <h2>Hello User!</h2>
+    <p>You are visitor number: <strong>${visitCount}</strong></p>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
